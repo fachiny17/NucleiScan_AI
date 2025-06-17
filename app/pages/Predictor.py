da47ba9ddb9a79b9908355e4c5f2c2c1876e4398
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-06-17 13:31:37 +0100

    made an update

--- app/pages/Predictor.py
+++ app/pages/Predictor.py
@@ -157,9 +157,11 @@ def get_radar_chart(input_data):
 def add_predictions(input_data):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.dirname(os.path.dirname(current_dir))
-    model = os.path.join(project_root, "models", "logistic_regression_model.joblib")
-    scaler = os.path.join(project_root, "models", "scaler.joblib")    
-    
+    model_path = os.path.join(project_root, "models", "logistic_regression_model.joblib")
+    scaler_path = os.path.join(project_root, "models", "scaler.joblib")    
+    model = load(model_path)
+    scaler = load(scaler_path)
+
     input_array = np.array(list(input_data.values())).reshape(1, -1)
 
     input_array_scaled = scaler.transform(input_array)
