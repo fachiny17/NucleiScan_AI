bed891b0f5f5920a0a0f367a34c2877462e14488
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2025-06-17 12:38:30 +0100

    made an update:

--- app/pages/Predictor.py
+++ app/pages/Predictor.py
@@ -206,13 +206,13 @@ def main():
         st.switch_page("../app/NucleiScan_AI.py")
 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    css_path = os.path.join(current_dir, "NucleiScan_AI", "assets", "style.css")
+    css_path = os.path.join(current_dir, "assets", "style.css")
     with open(css_path) as f:
-        st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     nuclei_predict()
     if st.button("💬 Ask NucleiScan AI Assistant"):
         st.switch_page("pages/Clinical_Assistant.py")
         
 if __name__ == '__main__':
     main() 
-    #/home/fachiny17/codex/NucleiScan_AI/assets/style.css
+    /home/fachiny17/codex/NucleiScan_AI/assets/style.css
